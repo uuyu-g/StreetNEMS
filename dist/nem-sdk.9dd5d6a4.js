952d9af3032422c7cf8a1962868c268caea22b92
yutaro <aqueous.mkli@gmail.com>
2019-04-12 22:46:47 +0900

    不要なキャッシュを削除

--- dist/nem-sdk.9dd5d6a4.js
+++ dist/nem-sdk.9dd5d6a4.js
@@ -108477,7 +108477,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58714" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59193" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
