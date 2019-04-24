eb36308ebfe363e8ec074bd519118e4d9b3da520
yutaro <aqueous.mkli@gmail.com>
2019-04-24 15:25:48 +0900

    フォントファイルを追加

--- src/tagging.js
+++ src/tagging.js
@@ -8,7 +8,7 @@
 function tagging(address,pubKey) {
   const name = address.substr(0, 6);
   const num = parseInt(pubKey.substr(0, 2), 16);
-  return `${name} ${num}`;
+  return `${name}${num}`;
 }
 
 
